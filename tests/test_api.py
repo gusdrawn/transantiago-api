3def7b098ee1e3be763d56d9a5218760ec1dfa03
Ignacio Hermosilla <hermosillavenegas@gmail.com>
2017-12-14 22:04:31 +0000

    tests: update tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,53 +1,43 @@
 # -*- coding: utf-8 -*-
 
+import os
+import logging
+import arrow
 from falcon import testing
-from scl_transport.api.models import (
+from scl_transport.gtfsdb.gtfsdb import (
     Stop,
     Agency,
     Route,
     StopTime,
+    RouteStop,
     Trip,
-    Service
 )
-
 from scl_transport.api.api import create_app
-from scl_transport.database import Adapter, CombinedConnection, Model
+from scl_transport.gtfsdb.gtfsdb import Database
 
 
 class BaseTestCase(testing.TestCase):
     def setUp(self):
         super(BaseTestCase, self).setUp()
+        os.putenv('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres:@localhost/test_gtfs')
+        self.db = Database()
+        self.session = self.db.session
+        # createdb
+        self.db.create()
+        self.app = create_app()
+        logging.disable(logging.DEBUG)
 
-        # connect to db for use by test
-        self.cc = CombinedConnection()
-        self.cc.create_db(Model)
-
-        # clear all tables
-        self.cc.clear_tables(Model)
-
-        # open session
-        self.session = self.cc.get_session()
-
-        # create LEM adapter
-        self.adapter = Adapter(connection=self.cc)
         # return a `falcon.API` instance.
         self.app = create_app()
 
     def tearDown(self):
         # close session and connection
         self.session.close()
-        self.cc.close()
-
         # call super
         super(BaseTestCase, self).tearDown()
 
     def get(self, url, query_string=None):
         return self.simulate_get(url, query_string=query_string)
-
-"""
-    app.add_route('/v1/trips/', TripCollectionResource())
-    app.add_route('/v1/trips/{trip_id}', TripResource())  # TODO: order by sequence
-"""
 
 
 class TripStopsTestCase(BaseTestCase):
@@ -84,24 +74,12 @@ class TripStopsTestCase(BaseTestCase):
         )
         self.session.add(self.route)
 
-        self.service = Service(
-            service_id='L_V34',
-            monday=True,
-            tuesday=True,
-            wednesday=True,
-            thursday=True,
-            friday=True,
-            saturday=True,
-            sunday=True,
-            start_date=None,
-            end_date=None
-        )
-        self.session.add(self.service)
+        self.trip_id = '106-I-L_V34-B02'
 
         self.trip = Trip(
-            trip_id='106-I-L_V34-B02',
+            trip_id=self.trip_id,
             route_id=self.route.route_id,
-            service_id=self.service.service_id,
+            service_id='TS',
             shape_id=None,
             trip_headsign='La Florida',
             direction_id='0'
@@ -109,25 +87,24 @@ class TripStopsTestCase(BaseTestCase):
         self.session.add(self.trip)
 
         self.stop_time = StopTime(
-            trip_id='106-I-L_V34-B02',
+            trip_id=self.trip_id,
             arrival_time=None,
             departure_time=None,
             stop_id=self.stop.stop_id,
             stop_sequence=1
         )
-        self.session.add(self.stop_time)
         self.session.add(self.trip)
+        self.session.add(self.stop_time)
         self.session.commit()
 
     def test_collection(self):
-        result = self.get('/v1/trips/106-I-L_V34-B02/stops')
+        result = self.get('/v1/trips/{}/stops'.format(self.trip_id))
         self.assertEqual(result.status_code, 200)
 
 
 class TripsTestCase(BaseTestCase):
     def setUp(self):
         super(TripsTestCase, self).setUp()
-
         self.agency = Agency(
             agency_id='TS',
             agency_name='Transantiago',
@@ -135,6 +112,16 @@ class TripsTestCase(BaseTestCase):
             agency_timezone='America/Santiago'
         )
         self.session.add(self.agency)
+
+        self.stop = Stop(
+            stop_id='PA1',
+            stop_code='PA1',
+            stop_name='PA1-Parada 6 / (M) Quinta Normal',
+            stop_lat='-33.4401169274166',
+            stop_lon='-33.4401169274166',
+            stop_url='http://www.metro.cl/estacion/vicuna-mackenna-l4'
+        )
+        self.session.add(self.stop)
 
         self.route = Route(
             route_id='106',
@@ -147,31 +134,25 @@ class TripsTestCase(BaseTestCase):
 
         )
         self.session.add(self.route)
-
-        self.service = Service(
-            service_id='L_V34',
-            monday=True,
-            tuesday=True,
-            wednesday=True,
-            thursday=True,
-            friday=True,
-            saturday=True,
-            sunday=True,
-            start_date=None,
-            end_date=None
-        )
-        self.session.add(self.service)
-
         self.trip = Trip(
             trip_id='106-I-L_V34-B02',
             route_id=self.route.route_id,
-            service_id=self.service.service_id,
+            service_id='L_V34',
             shape_id=None,
             trip_headsign='La Florida',
             direction_id='0'
         )
 
+        self.stop_time = StopTime(
+            trip_id='106-I-L_V34-B02',
+            arrival_time=None,
+            departure_time=None,
+            stop_id='PA1',
+            stop_sequence=1
+        )
+
         self.session.add(self.trip)
+        self.session.add(self.stop_time)
         self.session.commit()
 
     def test_collection(self):
@@ -182,32 +163,15 @@ class TripsTestCase(BaseTestCase):
             u'page_size': 1,
             u'results': [{
                 u'direction_id': u'0',
+                u'end_time': None,
                 u'frequency': None,
-                u'route': {
-                    u'agency_id': u'TS',
-                    u'route_color': u'00D5FF',
-                    u'route_desc': None,
-                    u'route_id': u'106',
-                    u'route_long_name': u'Nueva San Martin - La Florida',
-                    u'route_short_name': u'106',
-                    u'route_text_color': u'000000',
-                    u'route_type': u'3',
-                    u'route_url': None
-                },
-                u'service': {
-                    u'end_date': None,
-                    u'friday': True,
-                    u'monday': True,
-                    u'saturday': True,
-                    u'service_id': u'L_V34',
-                    u'start_date': None,
-                    u'sunday': True,
-                    u'thursday': True,
-                    u'tuesday': True,
-                    u'wednesday': True
-                },
+                u'route_id': u'106',
+                u'service_id': u'L_V34',
+                u'start_time': None,
                 u'trip_headsign': u'La Florida',
-                u'trip_id': u'106-I-L_V34-B02'
+                u'trip_id': u'106-I-L_V34-B02',
+                u'trip_len': 1,
+                u'trip_short_name': None
             }],
             u'total_pages': 1,
             u'total_results': 1
@@ -219,32 +183,15 @@ class TripsTestCase(BaseTestCase):
         result = self.get('/v1/trips/106-I-L_V34-B02')
         expected_json = {
             u'direction_id': u'0',
+            u'end_time': None,
             u'frequency': None,
-            u'route': {
-                u'agency_id': u'TS',
-                u'route_color': u'00D5FF',
-                u'route_desc': None,
-                u'route_id': u'106',
-                u'route_long_name': u'Nueva San Martin - La Florida',
-                u'route_short_name': u'106',
-                u'route_text_color': u'000000',
-                u'route_type': u'3',
-                u'route_url': None
-            },
-            u'service': {
-                u'end_date': None,
-                u'friday': True,
-                u'monday': True,
-                u'saturday': True,
-                u'service_id': u'L_V34',
-                u'start_date': None,
-                u'sunday': True,
-                u'thursday': True,
-                u'tuesday': True,
-                u'wednesday': True
-            },
+            u'route_id': u'106',
+            u'service_id': u'L_V34',
+            u'start_time': None,
             u'trip_headsign': u'La Florida',
-            u'trip_id': u'106-I-L_V34-B02'
+            u'trip_id': u'106-I-L_V34-B02',
+            u'trip_len': 1,
+            u'trip_short_name': None
         }
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result.json, expected_json)
@@ -291,6 +238,8 @@ class RoutesTestCase(BaseTestCase):
             u'page_size': 1,
             u'results': [{
                 u'agency_id': u'TS',
+                u'directions': [],
+                u'end_date': None,
                 u'route_color': u'00D5FF',
                 u'route_desc': None,
                 u'route_id': u'106',
@@ -298,12 +247,12 @@ class RoutesTestCase(BaseTestCase):
                 u'route_short_name': u'106',
                 u'route_text_color': u'000000',
                 u'route_type': u'3',
-                u'route_url': None
+                u'route_url': None,
+                u'start_date': None
             }],
             u'total_pages': 1,
             u'total_results': 1
         }
-
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result.json, expected_json)
 
@@ -311,6 +260,8 @@ class RoutesTestCase(BaseTestCase):
         result = self.get('/v1/routes/106')
         expected_json = {
             u'agency_id': u'TS',
+            u'directions': [],
+            u'end_date': None,
             u'route_color': u'00D5FF',
             u'route_desc': None,
             u'route_id': u'106',
@@ -318,7 +269,8 @@ class RoutesTestCase(BaseTestCase):
             u'route_short_name': u'106',
             u'route_text_color': u'000000',
             u'route_type': u'3',
-            u'route_url': None
+            u'route_url': None,
+            u'start_date': None
         }
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result.json, expected_json)
@@ -347,12 +299,12 @@ class StopsTestCase(BaseTestCase):
             u'page_number': 1,
             u'page_size': 1,
             u'results': [{
+                u'agency_id': None,
                 u'stop_code': u'PA1',
                 u'stop_id': u'PA1',
-                u'stop_lat': u'-33.4401169274166',
-                u'stop_lon': u'-33.4401169274166',
-                u'stop_name': u'PA1-Parada 6 / (M) Quinta Normal',
-                u'stop_url': u'http://www.metro.cl/estacion/vicuna-mackenna-l4'
+                u'stop_lat': u'-33.440116927',
+                u'stop_lon': u'-33.440116927',
+                u'stop_name': u'PA1-Parada 6 / (M) Quinta Normal'
             }],
             u'total_pages': 1,
             u'total_results': 1
@@ -363,12 +315,12 @@ class StopsTestCase(BaseTestCase):
     def test_item(self):
         result = self.get('/v1/stops/PA1')
         expected_json = {
+            u'agency_id': None,
             u'stop_code': u'PA1',
             u'stop_id': u'PA1',
-            u'stop_lat': u'-33.4401169274166',
-            u'stop_lon': u'-33.4401169274166',
-            u'stop_name': u'PA1-Parada 6 / (M) Quinta Normal',
-            u'stop_url': u'http://www.metro.cl/estacion/vicuna-mackenna-l4'
+            u'stop_lat': u'-33.440116927',
+            u'stop_lon': u'-33.440116927',
+            u'stop_name': u'PA1-Parada 6 / (M) Quinta Normal'
         }
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result.json, expected_json)
@@ -408,24 +360,20 @@ class StopRoutesTestCase(BaseTestCase):
         )
         self.session.add(self.route)
 
-        self.service = Service(
-            service_id='L_V34',
-            monday=True,
-            tuesday=True,
-            wednesday=True,
-            thursday=True,
-            friday=True,
-            saturday=True,
-            sunday=True,
-            start_date=None,
-            end_date=None
+        self.route_stop = RouteStop(
+            route_id=self.route.route_id,
+            direction_id=0,
+            stop_id=self.stop.stop_id,
+            order=1,
+            start_date=arrow.now().date(),
+            end_date=arrow.now().date(),
         )
-        self.session.add(self.service)
+        self.session.add(self.route_stop)
 
         self.trip = Trip(
             trip_id='106-I-L_V34-B02',
             route_id=self.route.route_id,
-            service_id=self.service.service_id,
+            service_id='L_V34',
             shape_id=None,
             trip_headsign='La Florida',
             direction_id='0'
@@ -443,11 +391,14 @@ class StopRoutesTestCase(BaseTestCase):
         self.session.commit()
 
     def test_collection(self):
-        result = self.get('/v1/stops/PA1/routes')
+        result = self.get('/v1/stops/PA1/stop_routes')
         self.assertEqual(result.status_code, 200)
-        expected_json = {
-            u'results': [{
+        expected_json = [{
+            u'direction': None,
+            u'route': {
                 u'agency_id': u'TS',
+                u'directions': [],
+                u'end_date': None,
                 u'route_color': u'00D5FF',
                 u'route_desc': None,
                 u'route_id': u'106',
@@ -455,9 +406,10 @@ class StopRoutesTestCase(BaseTestCase):
                 u'route_short_name': u'106',
                 u'route_text_color': u'000000',
                 u'route_type': u'3',
-                u'route_url': None
-            }]
-        }
+                u'route_url': None,
+                u'start_date': None
+            }
+        }]
         self.assertEqual(result.json, expected_json)
 
 

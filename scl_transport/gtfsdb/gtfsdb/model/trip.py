1216c9729132a0f679baeffe94e516969d45d411
Ignacio Hermosilla <hermosillavenegas@gmail.com>
2017-12-24 15:11:13 +0000

    feat(trip): method to get data for "active" trip (trip, stop_times, is_active, cache expiration in seconds)

--- scl_transport/gtfsdb/gtfsdb/model/trip.py
+++ scl_transport/gtfsdb/gtfsdb/model/trip.py
@@ -1,12 +1,16 @@
 import logging
-log = logging.getLogger(__name__)
 
 from sqlalchemy import Column
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import Integer, String
 
+from datetime import datetime, date
+
 from ..settings import config
+from ..util import get_now_datetime
 from .base import Base
+
+log = logging.getLogger(__name__)
 
 
 class Trip(Base):
@@ -107,6 +111,49 @@ class Trip(Base):
         from .stop_time import StopTime
         stop_times = StopTime.get_active_stop_times_for_route(session, route_id, direction_id)
         return [stop_time.trip for stop_time in stop_times]
+
+    @classmethod
+    def get_trip_data_for_route(cls, session, route_id, direction_id):
+        from .stop_time import StopTime
+        from .frequency import Frequency
+        now = get_now_datetime()
+        # get stop times for <route_id>, <direction_id>
+        trips = session.query(Trip).filter(Trip.route_id == route_id, Trip.direction_id == int(direction_id))
+        trip_ids = [trip.trip_id for trip in trips]
+        stop_times_results = StopTime.get_departure_schedule(session, date=now.date(), trip_ids=trip_ids)
+
+        # filter stop times on Frequency
+        stop_times = session.query(StopTime).join(Trip, Trip.trip_id == StopTime.trip_id).filter(
+            Trip.trip_id.in_(trip_ids)
+        )
+        stop_times = stop_times.join(StopTime.frequency).filter(
+            StopTime.id.in_([stop_time.id for stop_time in stop_times_results]),
+            Frequency.start_time <= now.time(),
+            Frequency.end_time >= now.time()
+        )
+        # fallback: get an arbitrary trip (is_active=False) if there is not an active trip
+        if not stop_times.count():
+            cache_expiration = 60  # the result is only valid 60 in this case
+            is_active = False
+            trip = session.query(Trip).filter(
+                Trip.route_id == route_id,
+                Trip.direction_id == int(direction_id)
+            ).first()
+            stop_times = session.query(StopTime).filter(
+                StopTime.trip_id == trip.trip_id).order_by(StopTime.stop_sequence)
+            return trip, stop_times, is_active, 60
+        else:
+            is_active = True
+            trip = stop_times.first().trip
+            # check if extra filter for stoptimes is required
+            if stop_times.distinct(StopTime.trip_id).count() > 1:
+                stop_times = stop_times.filter(StopTime.trip_id == trip.trip_id)
+            # get validity (cache) in seconds
+            frequency = session.query(Frequency).filter(Frequency.trip_id == trip.trip_id).first()
+            end_t = datetime.combine(date.today(), frequency.end_time)
+            now_t = datetime.combine(date.today(), now.time())
+            cache_expiration = (end_t - now_t).seconds
+            return trip, stop_times, is_active, cache_expiration
 
     @classmethod
     def get_trips_for_stop(cls, session, stop_id, route_id=None, only_active=False):
